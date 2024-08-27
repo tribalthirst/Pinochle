627c96e26de921289e513d47d52d135fe34f36b0
Tribal Thirst <mark.kola@live.com>
2024-08-26 20:11:45 -0700

    Card implemented with Tests

---
+++ src/Librarires/TribalThirst.Cards/Card.cs
@@ -0,0 +1,16 @@
+﻿namespace TribalThirst.Cards;
+
+public class Card
+{
+    public Card(string name, string shortName, Suit suit)
+    {
+        Name = !string.IsNullOrWhiteSpace(name) ? name : throw new ArgumentException("Card name cannot be empty", nameof(name));
+        ShortName = !string.IsNullOrWhiteSpace(shortName) ? shortName : throw new ArgumentException("Card short name cannot be empty", nameof(shortName));
+        Suit = suit ?? throw new ArgumentException("Card suit cannot be null", nameof(suit));
+    }
+
+    public string Name { get; }
+    public Suit Suit { get; }
+    public string ShortName { get; }
+
+}
---
+++ src/Tests/TribalThirst.Cards.Tests/Card_Tests.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+
+namespace TribalThirst.Cards.Tests;
+
+[TestClass]
+public class Card_Tests
+{
+    [DataRow("8", "")]
+    [DataRow("", "8")]
+    [DataRow("", "")]
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void Card_Missing_Required_Fields_Throws_Exception(string name, string shortName)
+    {
+        Suit diamonds = new("Diamonds", "♦️", Color.Red);
+        
+        Card card = new(name,shortName, diamonds);
+    }
+
+    [TestMethod]
+    public void Card_Name_ShortName_Suit_With_Value()
+    {
+        Suit diamonds = new("Diamonds", "♦️", Color.Red);
+        
+        Card card = new("Eight", "8", diamonds);
+        
+        Assert.AreEqual("Eight", card.Name);
+        Assert.AreEqual("8", card.ShortName);
+        Assert.AreEqual(diamonds, card.Suit);
+    }
+}
