cbf7d10af998292ad75ead154b9514573a5572de
Tribal Thirst <mark.kola@live.com>
2024-08-29 18:14:27 -0700

    Adding Value Object Card and Suit Package add of CSharpFunctionalExtensions and moved includes to the project

---
+++ src/Libraries/TribalThirst.Cards/Card.cs
@@ -0,0 +1,34 @@
+﻿
+namespace TribalThirst.Cards;
+
+public class Card : ValueObject
+{
+
+    public string Name { get; private set; }
+    public Suit Suit { get; private set; }
+    public string ShortName { get; private set; }
+    private Card(string name, string shortName, Suit suit)
+    {
+        Name = name;
+        ShortName = shortName;
+        Suit = suit;
+    }
+
+    public static Card Create(string name, string shortName, Suit suit)
+    {
+        return string.IsNullOrWhiteSpace(name)
+            ? throw new ArgumentException("Card name cannot be empty", nameof(name))
+            : string.IsNullOrWhiteSpace(shortName)
+            ? throw new ArgumentException("Card short name cannot be empty", nameof(shortName))
+            : suit is null
+            ? throw new ArgumentException("Card suit cannot be null", nameof(suit))
+            : new Card(name, shortName, suit);
+    }
+
+    protected override IEnumerable<IComparable> GetEqualityComponents()
+    {
+        yield return Name;
+        yield return ShortName;
+        yield return Suit;
+    }
+}
---
+++ src/Libraries/TribalThirst.Cards/Suit.cs
@@ -0,0 +1,39 @@
+﻿using static System.Char;
+using static System.String;
+
+namespace TribalThirst.Cards;
+
+
+public class Suit : ValueObject
+{
+    public string Name { get; private set; }
+    public string Symbol { get; private set; }
+    public Color Color { get; private set; }
+
+    private Suit(string name, string symbol, Color color)
+    {
+        Name = name;
+        Symbol = symbol;
+        Color = color;
+    }
+
+    public static Suit Create(string name, string symbol, Color color)
+    {
+        return IsNullOrWhiteSpace(name)
+            ? throw new ArgumentException("Suit name cannot be empty", nameof(name))
+            : !IsValidSymbol(symbol)
+            ? throw new ArgumentException("Suit Symbol is not valid")
+            : new(name, symbol, color);
+    }
+
+    private static bool IsValidSymbol(string symbol) =>
+        !IsNullOrWhiteSpace(symbol)
+        && (symbol.Length == 1 || (symbol.Length == 2 && IsSymbol(symbol[0])));
+
+    protected override IEnumerable<IComparable> GetEqualityComponents()
+    {
+        yield return Name;
+        yield return Symbol;
+        yield return Color.Name;
+    }
+}
