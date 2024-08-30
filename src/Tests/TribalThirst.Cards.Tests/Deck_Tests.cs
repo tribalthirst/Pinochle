be01417dbf379284bd2ab81cab1e652c5ba329e3
Tribal Thirst <mark.kola@tribalthirst.com>
2024-08-29 20:52:01 -0700

    Adding Extra Unit test

--- src/Tests/TribalThirst.Cards.Tests/Deck_Tests.cs
+++ src/Tests/TribalThirst.Cards.Tests/Deck_Tests.cs
@@ -24,5 +24,24 @@
             Assert.AreEqual(deck1, deck2);
         }
 
+        [TestMethod]
+        public void Pinochle_Deck_Contains_Correct_Cards()
+        {
+            Deck deck = PinochleDeck.Create();
+            Assert.AreEqual(48, deck.Cards.Count);
+            Assert.AreEqual(4, deck.Suits.Count);
+            Assert.AreEqual(DeckType.Pinochle, deck.DeckType);
+            Assert.AreEqual(12, deck.Cards.Count(c => c.Suit.Name == "Diamonds"));
+            Assert.AreEqual(12, deck.Cards.Count(c => c.Suit.Name == "Hearts"));
+            Assert.AreEqual(12, deck.Cards.Count(c => c.Suit.Name == "Clubs"));
+            Assert.AreEqual(12, deck.Cards.Count(c => c.Suit.Name == "Spades"));
+            Assert.AreEqual(8, deck.Cards.Count(c => c.Name == "Nine"));
+            Assert.AreEqual(8, deck.Cards.Count(c => c.Name == "Jack"));
+            Assert.AreEqual(8, deck.Cards.Count(c => c.Name == "Queen"));
+            Assert.AreEqual(8, deck.Cards.Count(c => c.Name == "King"));
+            Assert.AreEqual(8, deck.Cards.Count(c => c.Name == "Ten"));
+            Assert.AreEqual(8, deck.Cards.Count(c => c.Name == "Ace"));
+        }
+
     }
 }
